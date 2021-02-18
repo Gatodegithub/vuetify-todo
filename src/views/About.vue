db55fbd938b5b4be81a92dd5be3c63a180cdb0bb
leandro <aldoleandrosaavedramorales@gmail.com>
2021-02-18 16:18:06 -0300

    fin

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, ipsa adipisci recusandae accusantium aperiam ratione hic explicabo cum voluptatem? Distinctio ipsum quibusdam doloribus dolorem, assumenda dicta. Aut, cupiditate iusto. Recusandae!
+    Fugit, tenetur doloribus, iure ex explicabo consectetur optio earum, facere maiores quod asperiores accusantium animi et. A obcaecati incidunt accusamus fugit quos! Quae neque dolore laborum in officia voluptatum ad.
+    In cupiditate, facilis sunt voluptatem iusto deserunt quaerat voluptatibus necessitatibus iure, maxime reiciendis sequi delectus voluptates quis laudantium placeat porro cum praesentium dicta voluptas expedita facere, eos maiores. Velit, voluptas.
+    Quod non, sed recusandae fuga fugiat totam nam enim quae quaerat, nihil ipsum dolore amet alias repellendus obcaecati iure accusamus blanditiis! Eligendi quidem ipsum, enim dicta fugit libero natus sapiente.</p>
   </div>
 </template>
